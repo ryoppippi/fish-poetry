45076e31561437e1df18573ec7e47781220e9b25
Jens Langhammer <jens.langhammer@beryju.org>
2025-02-01 02:26:39 +0100

    fix indent

--- conf.d/fish-poetry.fish
+++ conf.d/fish-poetry.fish
@@ -26,21 +26,21 @@ if command -s poetry > /dev/null
         end
 
         if not test -n "$POETRY_ACTIVE"
-          if poetry env info -p >/dev/null 2>&1
-            set -x __poetry_fish_initial_pwd "$PWD"
-            if test "$FISH_POETRY_LOAD_ENV" -a -e "$PWD/.env"
-                echo "Setting environment variables..."
-                posix-source $PWD/.env
-            end
+            if poetry env info -p >/dev/null 2>&1
+                set -x __poetry_fish_initial_pwd "$PWD"
+                if test "$FISH_POETRY_LOAD_ENV" -a -e "$PWD/.env"
+                    echo "Setting environment variables..."
+                    posix-source $PWD/.env
+                end
 
-            poetry shell
+                poetry shell
 
-            set -e __poetry_fish_initial_pwd
-            if test -n "$__poetry_fish_final_pwd"
-                cd "$__poetry_fish_final_pwd"
-                set -e __poetry_fish_final_pwd
+                set -e __poetry_fish_initial_pwd
+                if test -n "$__poetry_fish_final_pwd"
+                    cd "$__poetry_fish_final_pwd"
+                    set -e __poetry_fish_final_pwd
+                end
             end
-          end
         end
     end
 else
